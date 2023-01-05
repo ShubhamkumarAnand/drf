e8a1d1e5c48746546c4adad5d2651a65b4ca9682
imskanand <the.anand3099@gmail.com>
2023-01-05 23:35:24 +0530

    Created an 'api/' route in Django Project -  created first Api endpoint - tested with the py_client

--- py_client/basic.py
+++ py_client/basic.py
@@ -2,14 +2,16 @@ import requests
 
 # endpoint = "https://httpbin.org/anything"
 
-endpoint = "http://localhost:8000/"
-get_response = requests.get(endpoint, data={"query": "Hello World"}) #HTTP Request
+endpoint = "http://localhost:8000/api"
+get_response = requests.get(
+    endpoint, data={"query": "Hello World"})  # HTTP Request
 
 
 # HTTP Request -> HTML
 # REST API HTTP Request -> JSON
 # JavaScript Object Notation ~ Python Dictionary
 
-# print(get_response.text)
-# print(get_response.json())
+print(get_response.text)
+print(get_response.json())
+print(get_response.json()['message'])
 print(get_response.status_code)

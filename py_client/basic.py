deafbc94ba0cb5bdae59b5defe6f3ad48de646c7
imskanand <the.anand3099@gmail.com>
2023-01-05 23:19:05 +0530

    Running the Django Project and calling the status code from the py_client

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,13 +1,15 @@
 import requests
 
-endpoint = "https://httpbin.org/anything"
+# endpoint = "https://httpbin.org/anything"
 
+endpoint = "http://localhost:8000/"
 get_response = requests.get(endpoint, data={"query": "Hello World"}) #HTTP Request
+
 
 # HTTP Request -> HTML
 # REST API HTTP Request -> JSON
 # JavaScript Object Notation ~ Python Dictionary
 
-print(get_response.text)
-print(get_response.json())
+# print(get_response.text)
+# print(get_response.json())
 print(get_response.status_code)

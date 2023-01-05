6d96381b58117633861ecbe03cd77aab5e3dae1a
imskanand <the.anand3099@gmail.com>
2023-01-06 00:59:46 +0530

    Echo GET data

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,19 @@
+import json
 from django.http import JsonResponse
 
 
 def api_home(request, *args, **kwargs):
-    return JsonResponse({
-        "message": "Hi there! From Django here is your API response"
-    })
+    # request -> HTTP request -> Django request
+    # request.body
+    body = request.body
+    print(request.GET)  # Url Query Parameters
+    data = {}
+    try:
+        data = json.loads(body)
+    except:
+        pass
+    print(data)
+    data['params'] = dict(request.GET)
+    data['headers'] = dict(request.headers)
+    data['content_type'] = request.content_type
+    return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -2,16 +2,14 @@ import requests
 
 # endpoint = "https://httpbin.org/anything"
 
-endpoint = "http://localhost:8000/api"
+endpoint = "http://localhost:8000/api/"
 get_response = requests.get(
-    endpoint, data={"query": "Hello World"})  # HTTP Request
+    endpoint, params={"abc": 123}, json={"query": "Hello World"})  # HTTP Request
 
 
 # HTTP Request -> HTML
 # REST API HTTP Request -> JSON
 # JavaScript Object Notation ~ Python Dictionary
 
-print(get_response.text)
 print(get_response.json())
-print(get_response.json()['message'])
-print(get_response.status_code)
+# print(get_response.status_code)

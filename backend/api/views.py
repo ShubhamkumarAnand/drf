66d4dd836821e72683b676e6333f53dceff31ea5
imskanand <the.anand3099@gmail.com>
2023-01-06 13:26:35 +0530

    Created a new app Products Returned Django Model Instance as an API Response

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,19 +1,14 @@
 import json
 from django.http import JsonResponse
+from products.models import Product
 
 
 def api_home(request, *args, **kwargs):
-    # request -> HTTP request -> Django request
-    # request.body
-    body = request.body
-    print(request.GET)  # Url Query Parameters
+    model_data = Product.objects.all().order_by('?').first()
     data = {}
-    try:
-        data = json.loads(body)
-    except:
-        pass
-    print(data)
-    data['params'] = dict(request.GET)
-    data['headers'] = dict(request.headers)
-    data['content_type'] = request.content_type
+    if model_data:
+        data['id'] = model_data.id
+        data['title'] = model_data.title
+        data['content'] = model_data.content
+        data['price'] = model_data.price
     return JsonResponse(data)
